3767411711547b4991b6267621f6ce3699861759
SUMIT SINGH <44521361+mssg12@users.noreply.github.com>
2023-10-24 17:25:30 +0530

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+
+const homeController = require('../controller/homeController');
+const fileController = require('../controller/fileController');
+const upload = multer({dest: 'uploads/files'});
+
+router.get('/',homeController.home);
+router.post('/upload',upload.single('file'),fileController.upload);
+router.get('/view/:id',fileController.view);
+router.get('/delete/:id',fileController.delete);
+
+module.exports = router;
